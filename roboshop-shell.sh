adb1ba80d260342e76986505cf0490a9e6060b03
prathyusha <chollangiprathyusha@gmail.com>
2026-02-14 01:24:57 +0530

    changes are done

---
+++ roboshop-shell.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SG_ID="sg-059610db1385bb7db" #replace with your id 
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do 
+   INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyNewInstance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+    if [ $instance == "frontend" ];then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text 
+        
+        )
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instance[].PrivateIpAddress' \
+            --output text
+        )
+    fi
+done
