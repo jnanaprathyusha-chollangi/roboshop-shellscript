7db61e256b2c498a2abb119c2a7f672107432af2
prathyusha <chollangiprathyusha@gmail.com>
2026-02-14 01:40:34 +0530

    changes are done

--- roboshop-shell.sh
+++ roboshop-shell.sh
@@ -9,7 +9,7 @@ do
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyNewInstance}]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
     if [ $instance == "frontend" ];then
