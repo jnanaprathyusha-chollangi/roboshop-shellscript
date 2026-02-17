d12f769ccb5d052b63dd241ce4dff15a45c1eeac
prathyusha <chollangiprathyusha@gmail.com>
2026-02-17 15:27:14 +0530

    changes are done

--- catalogue.sh
+++ catalogue.sh
@@ -33,15 +33,18 @@ dnf module enable nodejs:20 -y &>>$LOGS_FILE
 VALIDATE $? "Enabling NodeJs Default 20"
 
 dnf install nodejs -y &>>$LOGS_FILE
-VALIDATE $? "Install Node"
+VALIDATE $? "Install NodeJS"
 
-id roboshop &>>$LOGS_FILE
-if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-    VALIDATE $? "Creating system user"
-else 
-    echo -e "Roboshop user already exist..$Y Skipping $N"
-fi
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+VALIDATE $? "Creating system user"
+
+#id roboshop &>>$LOGS_FILE
+#if [ $? -ne 0 ]; then
+ #   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+  #  VALIDATE $? "Creating system user"
+#else 
+  #  echo -e "Roboshop user already exist..$Y Skipping $N"
+#fi
 
 mkdir -p /app 
 VALIDATE $? "Creating app directroy"
@@ -49,22 +52,22 @@ VALIDATE $? "Creating app directroy"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGS_FILE
 VALIDATE $? "Downloading catalogue code"
 
-cd /app
-VALIDATE $? "Moving to app directory" 
+#cd /app
+#VALIDATE $? "Moving to app directory" 
 
-rm -rf /app/*
-VALIDATE $? "Removing existing code"
+#rm -rf /app/*
+#VALIDATE $? "Removing existing code"
 
-unzip /tmp/catalogue.zip &>>$LOGS_FILE
-VALIDATE $? "unzip catalogue code"
+#unzip /tmp/catalogue.zip &>>$LOGS_FILE
+#VALIDATE $? "unzip catalogue code"
 
-npm install &>>$LOGS_FILE
-VALIDATE $? "Install Dependenices"
+#npm install &>>$LOGS_FILE
+#VALIDATE $? "Install Dependenices"
 
-cp $SCRIPT_DIR/catalogue-service /etc/systemd/system/catalogue.service
-VALIDATE $? "Created systemctl service"
+#cp $SCRIPT_DIR/catalogue-service /etc/systemd/system/catalogue.service
+#VALIDATE $? "Created systemctl service"
 
-systemctl daemon-reload
-systemctl enable catalogue &>>$LOGS_FILE
-systemctl start 
-VALIDATE $? "starting and enabling catalogue"
+#systemctl daemon-reload
+#systemctl enable catalogue &>>$LOGS_FILE
+#systemctl start 
+#VALIDATE $? "starting and enabling catalogue"
