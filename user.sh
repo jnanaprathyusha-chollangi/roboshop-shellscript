da1cd694e9e391cef9ad367361c9a6bc13f58ffb
prathyusha <chollangiprathyusha@gmail.com>
2026-02-18 12:37:37 +0530

    user file update

---
+++ user.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop-shellscript"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devopsd88s.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS Default version"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJs Default 20"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
+
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else 
+    echo -e "Roboshop user already exist..$Y Skipping $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directroy"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading user code"
+
+cd /app
+VALIDATE $? "Moving to app directory" 
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/user.zip &>>$LOGS_FILE
+VALIDATE $? "unzip user code"
+
+npm install &>>$LOGS_FILE ## you can write, if node js is install are not if not install , if yes install..
+VALIDATE $? "Install Dependenices"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable user &>>$LOGS_FILE
+systemctl start user
+VALIDATE $? "starting and enabling user"
+
+
+
