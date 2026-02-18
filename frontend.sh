76afcac2d88ac991de732ad74a77f3188a9bcfda
prathyusha <chollangiprathyusha@gmail.com>
2026-02-18 18:09:33 +0530

    frontend file update

--- frontend.sh
+++ frontend.sh
@@ -40,12 +40,12 @@ VALIDATE $? "Revoming the defalut content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOGS_FILE
 VALIDATE $? "Downloaded and unzipped frontend"
 
 rm -rf /etc/nginx/nginx.conf
 
-cp $SCRIPT_DIR/nginx.conf / etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf / etc/nginx/nginx.conf &>>$LOGS_FILE
 VALIDATE $? "Copied our nginx conf file"
 
 
