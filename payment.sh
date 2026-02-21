d7d0bfecc98eccfffd3c621cbdd02382b8aeb4ee
prathyusha <chollangiprathyusha@gmail.com>
2026-02-21 18:37:06 +0530

    payment file changes are done

--- payment.sh
+++ payment.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/roboshop-shellscript"
+LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
-MYSQL_HOST=mysql.devopsd88s.online
+MYSQL_HOST=mysql.daws88s.online
+
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
@@ -26,34 +27,34 @@ VALIDATE(){
 }
 
 dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
-VALIDATE $? "installing the python"
+VALIDATE $? "Installing Python"
 
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
     VALIDATE $? "Creating system user"
-else 
-    echo -e "Roboshop user already exist..$Y Skipping $N"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app 
-VALIDATE $? "Creating app directroy"
+VALIDATE $? "Creating app directory"
 
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOGS_FILE
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOGS_FILE
 VALIDATE $? "Downloading payment code"
 
 cd /app
-VALIDATE $? "Moving to app directory" 
+VALIDATE $? "Moving to app directory"
 
 rm -rf /app/*
 VALIDATE $? "Removing existing code"
 
 unzip /tmp/payment.zip &>>$LOGS_FILE
-VALIDATE $? "unzip payment code"
+VALIDATE $? "Uzip payment code"
 
 cd /app 
 pip3 install -r requirements.txt &>>$LOGS_FILE
-VALIDATE $? "Installing dependiences"
+VALIDATE $? "Installing dependencies"
 
 cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 VALIDATE $? "Created systemctl service"
@@ -61,4 +62,4 @@ VALIDATE $? "Created systemctl service"
 systemctl daemon-reload
 systemctl enable payment &>>$LOGS_FILE
 systemctl start payment
-VALIDATE $? "starting and enabling payment"
+VALIDATE $? "Enabled and started payment"
